1000868c5c08f91b7d38d4ccb22b205d92afaa2e
Katerina_Katsarou <katekats@users.noreply.github.com>
2017-08-17 23:49:10 +0200

    Update log_regression.py

--- log_regression.py
+++ log_regression.py
@@ -41,14 +41,14 @@ def plot_confusion_matrix(cm, classes,
 #   where the records are classified in two classes
 # the first is the class with the bigger number of records and the other class includes
 #   the records of the rest classes with smaller number of records   
-def one_vs_rest(df):
-    df[295].unique()
+def one_vs_rest(df1):
+    df1[295].unique()
     class_samples = {}
  # for each class in column 295   
-    for i in df[295].unique(): 
+    for i in df1[295].unique(): 
 # find the records that belong in class i and put the pair class-number of records
 # in a dictionary    
-        class_samples.update({i:(len(df[df[295] == i].axes[0]))})
+        class_samples.update({i:(len(df1[df1[295] == i].axes[0]))})
     global maximum 
 # find the class with the max number of records    
     maximum = max(class_samples, key=class_samples.get)
@@ -59,12 +59,12 @@ def one_vs_rest(df):
     df1[295].replace(10, 0, inplace=True)
     df1[295].replace(11, 1, inplace=True)
 # asign to y the column 295 
-    y = df[[295]]
+    y = df1[[295]]
  # and drop column 295 from the df       
-    df.drop(df.columns[295], axis = 1, inplace = True)
+    df1.drop(df1.columns[295], axis = 1, inplace = True)
 # standardize the features of df  
-    std_scale = preprocessing.StandardScaler().fit(df)
-    df_std = std_scale.transform(df)
+    std_scale = preprocessing.StandardScaler().fit(df1)
+    df_std = std_scale.transform(df1)
     df_std = pd.DataFrame(df_std, columns = df.columns)
  # assign df to X       
     X = df_std
