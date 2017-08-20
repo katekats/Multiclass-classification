d0157f00cc7293ef946cc135167c1eef899651d8
Katerina_Katsarou <katekats@users.noreply.github.com>
2017-08-20 22:25:18 +0200

    Update log_regression.py

--- log_regression.py
+++ log_regression.py
@@ -41,58 +41,68 @@ def plot_confusion_matrix(cm, classes,
 #   where the records are classified in two classes
 # the first is the class with the bigger number of records and the other class includes
 #   the records of the rest classes with smaller number of records   
-def one_vs_rest(df1):
-    df1[295].unique()
+def one_vs_rest(df):
+# for the first 1000 records  
+    df1 = df.iloc[0:1000,:]
     class_samples = {}
- # for each class in column 295   
+ # for each class in column 295 
     for i in df1[295].unique(): 
 # find the records that belong in class i and put the pair class-number of records
 # in a dictionary    
-        class_samples.update({i:(len(df1[df1[295] == i].axes[0]))})
+      class_samples.update({i:(len(df1[df1[295] == i].axes[0]))})
     global maximum 
-# find the class with the max number of records    
-    maximum = max(class_samples, key=class_samples.get)
+# for the four possible binary classifications    
+    for j in range(4): 
+      df2 = df1.copy()
+# find the class with the max number of records
+      maximum = max(class_samples, key=class_samples.get)
 # if the class is this with the max number of records assign to it the value 1   
      # otherwise assign to the class the value 0    
-    df1[295].loc[df1[295] != maximum] = 10
-    df1[295].loc[df1[295] == maximum] = 11
-    df1[295].replace(10, 0, inplace=True)
-    df1[295].replace(11, 1, inplace=True)
+     # print (df2)
+      df2[295].loc[df2[295] != maximum] = 10
+      df2[295].loc[df2[295] == maximum] = 11
+      df2[295].replace(10, 0, inplace=True)
+      df2[295].replace(11, 1, inplace=True)
+     # print(df2)
 # asign to y the column 295 
-    y = df1[[295]]
+      y = df2[[295]]
  # and drop column 295 from the df       
-    df1.drop(df1.columns[295], axis = 1, inplace = True)
 # standardize the features of df  
-    std_scale = preprocessing.StandardScaler().fit(df1)
-    df_std = std_scale.transform(df1)
-    df_std = pd.DataFrame(df_std, columns = df.columns)
+      std_scale = preprocessing.StandardScaler().fit(df2.iloc[:, 0:295])
+      df_std = std_scale.transform(df2.iloc[:, 0:295])
+      df_std = pd.DataFrame(df_std)
  # assign df to X       
-    X = df_std
+      X = df_std
 #  use 10 folds for cross validation   
-    cv = KFold(n_splits=10)
+      cv = KFold(n_splits=10)
  # use logistic regression model with L1 with the parameters
-    log_model = linear_model.LogisticRegression(penalty='l1',solver='liblinear')
+      log_model = linear_model.LogisticRegression(penalty='l1',solver='liblinear')
  
  # use 10 cross validation for taining & testing the model   
-    for train, test in cv.split(X,y):
+      for train, test in cv.split(X,y):
         X_train, X_test = X.ix[train], X.ix[test]
         y_train, y_test = y.ix[train], y.ix[test]
  # training
-        log_model.fit(X_train.as_matrix(), y_train.as_matrix().ravel()) 
+        log_model.fit(X_train.as_matrix(),y_train.as_matrix().ravel()) 
 # testing
         y_predict = log_model.predict(X_test.as_matrix()) 
 # call the confusion matrix 
         cnf_matrix = confusion_matrix(y_test.as_matrix().ravel(), y_predict)
         print(accuracy_score(y_test.as_matrix().ravel(), y_predict))
 
-    np.set_printoptions(precision=2)
+      np.set_printoptions(precision=2)
 
 # Plot normalized confusion matrix
-    plt.figure()
-    plt.show()
-    plot_confusion_matrix(cnf_matrix, classes=[0, 1],
+      plt.figure()
+      plt.show()
+      plot_confusion_matrix(cnf_matrix, classes=[0, 1],
                       title='Normalized confusion matrix')
-    plt.show()
+      plt.show()
+      # drop the records that belongs to the maximum class
+      df1 = df1.drop(df1[df1[295] == maximum].index)
+      df1.index = range(len(df1.axes[0]))
+      # delete this class from the class_samples
+      del class_samples[maximum]
 #
      
 # read a csv file     
